8c647b58a78194768712d88314471a61faf558fc
skharat8 <skharat8@users.noreply.github.com>
2024-04-13 17:18:28 -0700

    Setup React testing environment

---
+++ src/App.test.tsx
@@ -0,0 +1,32 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+import { ReactElement } from "react";
+
+function setup(jsx: ReactElement) {
+  // Use setup functions that compose together when you have a lot of tests
+  // that do the same thing.
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  };
+}
+
+test("renders main page", () => {
+  const { container } = setup(<App />);
+  expect(container).toMatchSnapshot();
+});
+
+test("renders correct heading", () => {
+  setup(<App title="Hello World" />);
+  expect(screen.getByRole("heading").textContent).toMatch(/Hello World/i);
+});
+
+test("increments count after button click", async () => {
+  const { user } = setup(<App />);
+  const button = screen.getByRole("button", { name: "count 0" });
+
+  await user.click(button);
+  expect(button.textContent).toMatch(/^count 1$/i);
+});
