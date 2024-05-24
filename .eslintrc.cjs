26f78d1db76dbdc296190428bb809c725a6699ed
skharat8 <5607079+skharat8@users.noreply.github.com>
2024-05-23 19:36:05 -0700

    Update ES version in TypeScript & ESLint to ES2022

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ const alias = require("./alias.cjs");
 
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2022: true },
   extends: [
     "airbnb",
     "airbnb/hooks",
