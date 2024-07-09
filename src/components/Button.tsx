7e91cd61cddf29c2a8a046ab3880cfa98440a514
skharat8 <5607079+skharat8@users.noreply.github.com>
2024-07-09 00:06:57 -0700

    Refactor Button component to use objects for styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,18 +15,25 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     },
     ref,
   ) => {
-    let sizeClass;
-    if (size === "small") sizeClass = "px-6 py-3 text-xl uppercase mx-auto";
-    if (size === "medium") sizeClass = "px-12 py-6 text-2xl mx-auto";
-    if (size === "large") sizeClass = "px-20 py-8 text-4xl mx-auto";
+    const sizeStyles = {
+      small: "px-6 py-3 text-xl mx-auto uppercase",
+      medium: "px-12 py-6 text-2xl mx-auto",
+      large: "px-20 py-8 text-4xl mx-auto",
+    };
 
-    let variantClass;
-    if (variant === "primary") variantClass = styles.btnPrimary;
-    if (variant === "link") variantClass = styles.btnLink;
-    if (variant === "demo") variantClass = styles.btnGuestDemo;
-    if (variant === "small-caps") variantClass = styles.btnSmallCaps;
+    const variantStyles = {
+      primary: styles.btnPrimary,
+      link: styles.btnLink,
+      demo: styles.btnGuestDemo,
+      "small-caps": styles.btnSmallCaps,
+    };
 
-    const classNames = clsx(styles.btnBase, sizeClass, variantClass, className);
+    const classNames = clsx(
+      styles.btnBase,
+      size && sizeStyles[size],
+      variant && variantStyles[variant],
+      className,
+    );
 
     return (
       <button
@@ -48,7 +55,7 @@ Button.displayName = "Button";
 
 type ButtonProps = {
   type?: "button" | "submit" | "reset";
-  size?: "small" | "medium" | "large" | undefined;
+  size?: "small" | "medium" | "large";
   variant?: "primary" | "link" | "demo" | "small-caps";
   onClick?: () => void;
   disabled?: boolean;
