9e22ec383d2d29a5d35e728554eb4959a893c249
skharat8 <5607079+skharat8@users.noreply.github.com>
2024-07-12 22:50:55 -0700

    Redirect already authenticated users from login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Logo from "@/components/Logo";
 import Auth from "@/features/authentication/Auth";
+import useUser from "@/hooks/useUser";
 
 function Login() {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useUser();
+
+  useEffect(() => {
+    if (isAuthenticated) navigate("/", { replace: true });
+  }, [isAuthenticated, navigate]);
+
   return (
     <div className="centered-container">
       <h1 className="main-title">A modern Twitter experience</h1>
